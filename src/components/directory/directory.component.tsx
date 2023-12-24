6e60047e9ce48c343982d137625f7423c67b230c
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-23 22:38:57 -0800

    Added second basic directory for pinned notes.

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -5,12 +5,18 @@ import BasicDirectory from "../basic-directory/basic-directory.component";
 
 const Directory = () => {
     const {notes} = useContext(NotesContext)
-    const notesToDisplay = notes.filter(note => !note.markedForTrash && !note.markedForArchive)
-  
-    return <BasicDirectory
-        notes={notesToDisplay}
-        showNote={true}
-    />
+    const notesToDisplay = notes.filter(note => !note.markedForTrash && !note.markedForArchive && !note.notePinned)
+    const notesPinned = notes.filter(note => !note.markedForTrash && !note.markedForArchive && note.notePinned)
+
+    return (
+        <>
+            <BasicDirectory notes={notesPinned} showNote={false}/>
+            <BasicDirectory
+                notes={notesToDisplay}
+                showNote={true}
+            />
+        </>
+    )
 }
 
 export default Directory;
