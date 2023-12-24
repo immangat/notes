f78c7df89e7a30d021c6c06c05d4f17c72c6279b
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-23 22:37:16 -0800

    Deleted unneeded heading.

--- src/routes/archive/archive.component.tsx
+++ src/routes/archive/archive.component.tsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import {NotesContext} from "../../contexts/notes.context";
-import {BsFillTrashFill} from "react-icons/bs";
+import {BsArchive} from 'react-icons/bs'
 import BasicDirectory from "../../components/basic-directory/basic-directory.component";
 
 const Archive = () => {
@@ -15,7 +15,7 @@ const Archive = () => {
                 height: "100%",
                 flexDirection: "column"
             }}>
-                <BsFillTrashFill size={100}/>
+                <BsArchive size={100}/>
                 <h1>Your Archive is Empty </h1>
 
             </div>
@@ -23,12 +23,6 @@ const Archive = () => {
     }
     return (
         <div>
-            <h1
-                style={{
-                    textAlign: "center",
-                    margin: "0"
-                }}
-            >This is Archive</h1>
             <BasicDirectory notes={archive} showNote={false}/>
         </div>
     );
