ce31cc3cdde18c58f26ebf6717daa56a142fd806
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-24 18:50:39 -0800

    Updated search directory to use basic-directory.component.tsx.

--- src/components/search-directory/search-directory.component.tsx
+++ src/components/search-directory/search-directory.component.tsx
@@ -4,12 +4,8 @@ import {NotesContext} from "../../contexts/notes.context";
 import BasicDirectory, {NoteType} from "../basic-directory/basic-directory.component";
 import PreviewNote from "../preview-note/preview-note.component";
 import {
-    ContainerOfNoteContainer,
-    CreateNoteGridItem,
-    DirectoryContainer, NotesContainerTest,
-    NotesGridItem
+    ContainerOfNoteContainer, NotesContainerTest,
 } from "../basic-directory/basic-directory.styles";
-import CreateNote from "../create-note/create-note.component";
 import {SearchDirectoryContainer, SearchNotesGridItem} from "./search-directory.styles";
 
 type SearchDirectoryType = {
@@ -26,38 +22,18 @@ const SearchDirectory = (props: SearchDirectoryType) => {
     }
 
 
-    //const [number, setNumber] = useState(0)
-    const [notes1, setNotes1] = useState<ReactElement[]>([])
-    const deleteNoteFromArray = (key: string, array: number) => {
-        trashNote(key)
-    }
-
 
     useEffect(() => {
-        setNotes1([])
-        notes.forEach(noteContent => {
-                setNotes1(
-                    prevNotes => [...prevNotes, <PreviewNote
-                        key={noteContent.id}
-                        noteContent={noteContent}
-                        handleDelete={() => deleteNoteFromArray(noteContent.id, 1)}
-                    />]
-                )
-            }
-        )
+        if (!props.labelSearch) {
+            notes = getNotes(props.stringToSearch)
+        } else {
+            notes = getNotesBasedUponLabel(props.stringToSearch)
+        }
     }, [props])
 
 
     return (
-        <SearchDirectoryContainer>
-            <SearchNotesGridItem>
-                <ContainerOfNoteContainer>
-                    <NotesContainerTest>
-                        {notes1}
-                    </NotesContainerTest>
-                </ContainerOfNoteContainer>
-            </SearchNotesGridItem>
-        </SearchDirectoryContainer>
+        <BasicDirectory notes={notes} showNote={false}/>
     );
 }
 
