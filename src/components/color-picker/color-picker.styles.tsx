d743283c13d67a5a2fcfe2e82133c84df89ea122
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-25 16:17:39 -0800

    pointer for hover.

--- src/components/color-picker/color-picker.styles.tsx
+++ src/components/color-picker/color-picker.styles.tsx
@@ -5,12 +5,16 @@ type ColorPickerCircleType = {
     color: string
 }
 export const ColorPickerCircle = styled.div<ColorPickerCircleType>`
-    border:1px dashed black;
+    border: 1px dashed black;
     height: 25px;
     width: 25px;
     margin-left: 0.2rem;
     background-color: ${props => props.color};
     border-radius: 50%;
     display: inline-block;
+
+    &:hover {
+        cursor: pointer;
+    }
 
 `
