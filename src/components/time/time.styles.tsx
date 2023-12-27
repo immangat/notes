15a960c2f82d303cdf530edc3750de02c9d4f921
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-26 20:40:50 -0800

    added time component to show edited time

---
+++ src/components/time/time.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+
+export const TimeContainer = styled.div`
+    display: flex;
+    font-size: 10px;
+    justify-self: center;
+    align-items: center;
+    margin-left: auto;
+`
