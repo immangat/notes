6f3f83861dc5b95f0088383c0b48d1dc14aa7f43
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-28 14:34:30 -0800

    Added selected Notes code.

--- src/routes/nav-bar/nav-bar.component.tsx
+++ src/routes/nav-bar/nav-bar.component.tsx
@@ -8,10 +8,14 @@ import {useContext} from "react";
 import {NotesContext} from "../../contexts/notes.context";
 import SideBar from "../../components/side-bar/side-bar.component";
 import Navbar from "../../components/navbar/navbar.component";
+import {SelectNotesContext} from "../../contexts/select.context";
+import SelectNavbar from "../../components/select-narbar/select-navbar.component";
 
 
 const NavBar = () => {
     const {eventIncoming} = useContext(NotesContext)
+    const {selectedNotes} = useContext(SelectNotesContext)
+
     return (
         <MainContainer
             onClick={() => {
@@ -21,7 +25,12 @@ const NavBar = () => {
                 height: "100%"
             }}
         >
+
             <Navbar/>
+            <SelectNavbar
+                showSelectNavBar={selectedNotes.length === 0}
+            />
+
             <SideBarComponentContainer>
                 <SideBar/>
             </SideBarComponentContainer>
