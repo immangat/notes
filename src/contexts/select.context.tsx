e5b0d1f397c59967e358f7d57eb89a57400138c6
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-12-28 14:35:10 -0800

    Added functionality to remove all notes in selected notes.

--- src/contexts/select.context.tsx
+++ src/contexts/select.context.tsx
@@ -8,22 +8,27 @@ type SelectContextType = {
     selectedNotes: string[]
     addNoteToSelectedNotes: (noteID: string) => void,
     removeNoteFromSelectedNotes: (noteID: string) => void
+    removeAllNotesFromSelectedNotes: () => void
 }
 
 export const SelectNotesContext = createContext<SelectContextType>({
     selectedNotes: [],
     addNoteToSelectedNotes: (noteID: string) => null,
-    removeNoteFromSelectedNotes: (noteID: string) => null
+    removeNoteFromSelectedNotes: (noteID: string) => null,
+    removeAllNotesFromSelectedNotes: () => null
 })
 
 
 export const SelectNotesProvider = ({children}: SelectProviderPropsType) => {
 
     const [selectedNotes, setSelectedNotes] = useState<string[]>([])
-    console.log("notes", selectedNotes)
     const addNoteToSelectedNotes = (noteID: string) => setSelectedNotes(prev => [...prev, noteID])
     const removeNoteFromSelectedNotes = (noteID: string) => setSelectedNotes(prev => prev.filter(id => id !== noteID))
-    const value = {selectedNotes, addNoteToSelectedNotes, removeNoteFromSelectedNotes}
+
+    const removeAllNotesFromSelectedNotes = () => {
+        setSelectedNotes([])
+    }
+    const value = {selectedNotes, addNoteToSelectedNotes, removeNoteFromSelectedNotes, removeAllNotesFromSelectedNotes}
     return <SelectNotesContext.Provider value={value}>
         {children}
     </SelectNotesContext.Provider>
